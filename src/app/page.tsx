63d3e987ff07f254575bf0cf8447b9ef2ba7991c
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-12-25 21:54:26 +0500

    Nextjs Milestone 3 Project

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/components/Hero";
+import Testimonials from "@/components/Testimonials";
+import Newsletter from "@/components/NewsLetter";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Testimonials />
+      <Newsletter />
+    </>
+  );
+}
---
+++ src/components/Hero.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+
+const Hero = () => {
+    return (
+        <section className="relative bg-green-600 text-white p-2">
+            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                <div className="relative py-16 md:py-24">
+                    <div className="absolute inset-0 overflow-hidden">
+                        <svg
+                            className="absolute top-0 left-0 w-full h-full"
+                            preserveAspectRatio="xMidYMid slice"
+                            fill="none"
+                            viewBox="0 0 1463 502"
+                            aria-hidden="true"
+                        >
+                            <path
+                                fill="rgba(255, 255, 255, 0.1)"
+                                d="M-164 329L0 473l1463-1463-164-164L0 329z"
+                            />
+                        </svg>
+                    </div>
+                    <div className="relative text-center">
+                        <h1 className="text-[35px] font-extrabold tracking-tight sm:text-5xl">
+                            Welcome to BM-Shop
+                        </h1>
+                        <p className="mt-6 text-xl max-w-3xl text-black">
+                        Discover our exclusive range of products designed to elevate your shopping experience. Explore now for the best deals and seamless convenience!  Whether you are looking for trendy fashion, cutting-edge electronics, or home essentials, we have got you covered with unbeatable prices. 
+                        Shop with confidence, knowing that each item is carefully selected to meet the highest standards of quality. Enjoy easy navigation, fast checkout, and prompt delivery making your shopping journey a breeze.Don not miss out on the hottest products and exclusive offers! </p>                        
+                        <div className="mt-8">
+                            <Link
+                                href="/products"
+                                className="inline-block bg-gray-900 text-white py-2 px-6 rounded-md font-medium hover:bg-gray-800"
+                            >
+                                Shop Now
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
