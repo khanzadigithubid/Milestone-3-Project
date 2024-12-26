8879d1c5b528d36c06c28e12e0d46501d5948664
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-12-26 18:34:31 +0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,9 +6,9 @@ const AboutUs = () => {
         <section className="bg-green-600 py-12">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex flex-col-reverse lg:flex-row lg:items-center">
-                    <div className="lg:w-1/2 lg:pr-8 text-center">
-                        <h2 className="text-3xl font-extrabold text-white mt-6">About Us</h2>
-                        <p className="mt-4 text-lg text-black">
+                    <div className="lg:w-1/2 lg:pr-8">
+                        <h2 className="text-3xl font-extrabold text-white mt-6 text-center">About Us</h2>
+                        <p className="mt-4 text-lg text-black text-center">
                         Welcome to BM-Shop, your go-to destination for the best products and unbeatable deals! 🛍️✨
                         We are here to make your shopping experience effortless and fun, offering a wide variety of top-quality products at amazing prices. 💥💎
                          From fashion to electronics, beauty products, and everything in between, we have something for everyone! 👗📱💄
