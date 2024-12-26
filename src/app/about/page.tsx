dcf2d7f2dd3b31feb0095f54030b03f7bf2c1045
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-12-26 19:07:18 +0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,20 +1,17 @@
-
-
 const AboutUs = () => {
-    // About
     return (
         <section className="bg-green-600 py-12">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex flex-col-reverse lg:flex-row lg:items-center">
-                    <div className="lg:w-1/2 lg:pr-8">
-                        <h2 className="text-3xl font-extrabold text-white mt-6 text-center">About Us</h2>
-                        <p className="mt-4 text-lg text-black text-center">
-                        Welcome to BM-Shop, your go-to destination for the best products and unbeatable deals! 🛍️✨
-                        We are here to make your shopping experience effortless and fun, offering a wide variety of top-quality products at amazing prices. 💥💎
-                         From fashion to electronics, beauty products, and everything in between, we have something for everyone! 👗📱💄
-                          Our team is dedicated to providing you with excellent customer service, ensuring you feel supported every step of the way. 🤝💬
-                          Explore, shop, and enjoy a stress-free experience with us. We are always available to assist you with any questions or concerns. 🌟🙋‍♀️
-                         Join the BM-Shop today and experience the ultimate in shopping convenience! 🛒🎉
+                <div className="flex flex-col lg:items-center">
+                    <div className="lg:w-1/2 text-center">
+                        <h2 className="text-3xl font-extrabold text-white mt-6">About Us</h2>
+                        <p className="mt-4 text-lg text-gray-100">
+                            Welcome to BM-Shop, your go-to destination for the best products and unbeatable deals! 🛍️✨ 
+                            We are here to make your shopping experience effortless and fun, offering a wide variety of top-quality products at amazing prices. 💥💎 
+                            From fashion to electronics, beauty products, and everything in between, we have something for everyone! 👗📱💄 
+                            Our team is dedicated to providing you with excellent customer service, ensuring you feel supported every step of the way. 🤝💬 
+                            Explore, shop, and enjoy a stress-free experience with us. We are always available to assist you with any questions or concerns. 🌟🙋‍♀️ 
+                            Join the BM-Shop today and experience the ultimate in shopping convenience! 🛒🎉
                         </p>
                     </div>
                 </div>
