39a8a6555ea9573eb6ee9faf501ba3577c4346bf
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-12-30 02:33:49 +0500

    Update page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -33,7 +33,7 @@ const ProductsPage = () => {
     };
 
     return (
-        <div className="bg-green-600 min-h-screen py-12">
+        <div className="bg-green-300 min-h-screen py-12">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <h1 className="text-4xl font-bold text-center mb-8 text-white">Our Products</h1>
                 {!data ? (
